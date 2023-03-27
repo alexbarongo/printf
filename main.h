f1bb1d2a1262cf8b5a12e4d912d9c254c1f146b4
Robert Mawusi Kpodo <mawusikpodo@yahoo.com>
2023-03-27 14:25:59 -0700

    Added _prinf function

--- main.h
+++ main.h
@@ -1,30 +1,43 @@
-#ifndef MAIN_H_
-#define MAIN_H_
-
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef _MAIN_H_
+#define _MAIN_H_
 #include <stdarg.h>
-#include <limits.h>
-#include <unistd.h>
+#include <stdio.h>
 
 /**
- * struct format - match the conversion specifiers for printf
- * @id: type char pointer of the specifier
- * @f: type pointer to function for the conversion specifier
- *
+ * struct print - structure for printing various types
+ * @t: type to print
+ * @f: function to print
  */
-
-typedef struct format
+typedef struct print
 {
-	char *id;
-	int (*f)();
-} convert_match;
+	char *t;
+	int (*f)(va_list);
+} print_t;
 
-int _putchar(char c);
-int printBinary(va_list args);
-int printf_37(void);
-int printf_char(va_list val);
-int printf_string(va_list val);
 int _printf(const char *format, ...);
+int _putchar(char c);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
+int print_f(va_list f);
+int print_e(va_list e);
+int print_g(va_list g);
+int print_l(va_list l);
+int print_0(va_list zero);
+int print_h(va_list h);
+int print_u(va_list u);
+int print_b(va_list b);
+int print_o(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_p(va_list p);
+int print_S(va_list S);
+int print_r(va_list r);
+int print_R(va_list R);
+int print_add(va_list add);
+int print_less(va_list less);
+int print_space(va_list space);
+int print_sharp(va_list sharp);
 
 #endif
